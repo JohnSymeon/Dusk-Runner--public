e6b2c7b1a071bb5b939f5657751750255e5bfd51
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-06-11 20:56:46 +0300

    Create Authenticator.cs

---
+++ Authenticator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+using UnityEngine.SocialPlatforms;
+using System.Runtime.Serialization.Formatters.Binary;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class Authenticator : MonoBehaviour
+{
+
+    public bool connectedToGP;
+    void Awake()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+    GameController GC;
+    void Start()
+    {
+        
+        GC = FindObjectOfType<GameController>();
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    private void ProcessAuthentication(SignInStatus status)
+    {
+        if(status==SignInStatus.Success)
+            connectedToGP = true;
+        else
+            connectedToGP = false;
+    }
+
+    public void LeaderBoard()
+    {
+        if(!connectedToGP)
+            PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        else
+            Social.ShowLeaderboardUI();
+
+        if(connectedToGP)
+        {
+            Social.ReportScore(GC.currentscore, GPGSIds.leaderboard_dusk_runner, LeaderBoardUpdate);
+            Social.ShowLeaderboardUI();
+        }
+    }
+
+    private void LeaderBoardUpdate(bool sucess)
+    {
+        if(sucess)
+            Debug.Log("Updated High Score");
+        else
+            Debug.Log("Failed to update High Score");
+    }
+
+}
