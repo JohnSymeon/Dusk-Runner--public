2f30027ed518864a5d094aca38484072cd37dc91
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-06-11 20:57:59 +0300

    Create AudioManager.cs

---
+++ AudioManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+   void Start()
+   {
+        Play("Theme");
+   }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds,sound=>sound.name==name);
+        s.source.Play(); 
+    }
+
+    public void StopPlaying(string name)
+    {
+        Sound s = Array.Find(sounds,sound=>sound.name==name);
+        s.source.volume =0f;
+        s.source.Stop();
+    }
